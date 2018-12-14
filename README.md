0c299c65a7bb6994b2c3979b0404c7a753d99796
Daniel Mantovani <daniel@gmail.com>
2018-12-14 20:50:39 -0300

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojolicious::Plugin::Shared - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::Shared;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::Shared is ...
+
+# LICENSE
+
+Copyright (C) Daniel Mantovani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Daniel Mantovani <daniel@gmail.com>
---
+++ lib/Mojolicious/Plugin/Shared.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::Shared;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::Shared - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::Shared;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::Shared is ...
+
+=head1 LICENSE
+
+Copyright (C) Daniel Mantovani.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Daniel Mantovani E<lt>daniel@gmail.comE<gt>
+
+=cut
+
